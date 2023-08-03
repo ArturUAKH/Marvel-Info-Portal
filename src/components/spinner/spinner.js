9a5e4db37c3343932a58ab237e37ce9be20022ac
Artur <arti404049@gmail.com>
2023-08-03 15:30:56 +0300

    FIX: Wrong logic fixing

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,5 @@
+import spinner from './Spinner.svg';
+const Spinner = () => {
+    return <img src={spinner} alt="loading spinner" style={{ display: 'block', margin: '0 auto' }} />;
+};
+export default Spinner;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
 
-const marvelService = new MarvelService();
-
-marvelService.getCharacter(1011096).then(res => console.log(res.data.results[0].name));
-
 ReactDOM.render(
-   <React.StrictMode>
-      <App />
-   </React.StrictMode>,
-   document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
